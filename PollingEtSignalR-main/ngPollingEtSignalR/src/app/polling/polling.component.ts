d49393e7fc336950470d952b1a2d350817e1fb95
Gbecet Kouame Djahakpl <2164425@cegepmontpetit.ca>
2024-02-13 21:00:29 -0500

    PRogress:Pollign

--- PollingEtSignalR-main/ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ PollingEtSignalR-main/ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient,HttpParams} from "@angular/common/http";
 import { UselessTask } from '../models/UselessTask';
-import {HttpClient} from "@angular/common/http";
 import {lastValueFrom} from "rxjs";
+
+
 
 
 
@@ -11,37 +13,39 @@ import {lastValueFrom} from "rxjs";
   styleUrls: ['./polling.component.css']
 })
 export class PollingComponent implements OnInit {
-
+  constructor(public http:HttpClient) {
+  }
   title = 'labo.signalr.ng';
   tasks: UselessTask[] = [];
   uselessTask: UselessTask = new UselessTask(1,"",true)
 
 
 
-  taskname: string = "dwdw";
-constructor(public http:HttpClient) {
-}
+  taskname: string = "";
+
   ngOnInit(): void {
     this.updateTasks();
+
   }
 
-  complete(id: number) {
+  async complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
 
-    let response =  lastValueFrom(this.http.get<any>('https://localhost:7289/api/UselessTasks/Complete/'+ this.uselessTask.id));
+    let r = await lastValueFrom (this.http.get<any>('https://localhost:7289/api/UselessTasks/Complete/'+ id));
   }
 
-  addtask() {
+ async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
 
-    let response = lastValueFrom(this.http.post<any>('https://localhost:7289/api/UselessTasks/Add',this.taskname));
+     let r = await lastValueFrom(this.http.post<any>('https://localhost:7289/api/UselessTasks/Add?taskText='+ this.taskname,this.taskname))
 
+   this.tasks.push(new UselessTask(1,this.taskname,true))
   }
 
-  updateTasks() {
+  async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
-    let response = lastValueFrom(this.http.get<any>('https://localhost:7289/api/UselessTasks/GetAll'));
+     let r = await lastValueFrom(this.http.get<any>('https://localhost:7289/api/UselessTasks/GetAll'));
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
-
+    setTimeout(() => {this.updateTasks()}, 1);
   }
 }
