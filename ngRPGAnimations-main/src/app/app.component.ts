8a7c8a2cb6e1fc4c383024b26cc1d1aa4ce6cd74
2164425@cegpmontpetit.ca <2164425@cegepmontpetit.ca>
2024-03-27 15:27:02 -0400

    Progress:Animation

---
+++ ngRPGAnimations-main/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {transition, trigger, useAnimation} from "@angular/animations";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  slimeIsPresent = false;
+
+  constructor() {
+  }
+
+  spawn() {
+    this.slimeIsPresent = true;
+    // TODO Animation angular avec forwards
+
+  }
+
+  death(){
+    this.slimeIsPresent = false;
+    // TODO Animation angular avec forwards
+
+    // TODO 2e animation angular en même temps
+  }
+
+  attack(){
+    // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
+    // TODO Jouer une autre animation avant
+  }
+
+  hit(){
+    // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+  }
+
+  showSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+  }
+}
